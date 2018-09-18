cb832a8ffa26f278459bead0c8214c22794c197c
whitegu <whitegu@163.com>
2018-09-18 22:49:09 +0800

    第一次提交项目到GitHub

---
+++ testGit/src/main/java/Hello.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IDEA
+ * author:gu
+ * Date:2018/9/18
+ * Time:22:44
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("年号a ");
+    }
+}
